3a5a6fadb0584c445b41ba971c9e050b54edd197
revoboy <88243401+revoboy@users.noreply.github.com>
2021-08-09 11:32:25 +0300

    Add files via upload

---
+++ lab3.6/lab3.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab3._6
+{
+    class Program
+    {
+        public static int Main()
+        {
+            Console.Write("Для того, чтобы выяснить n! = введите значение n: \t\t НЕ БОЛЬШЕ 10!");
+            Console.WriteLine();
+
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+                factorial *= i;
+                if (i == n)
+                {
+                    Console.Write("{0}", i);
+                }
+                else
+                {
+                    Console.Write("{0} * ", i);
+                }
+            }
+
+            Console.Write(" = {0}", factorial);
+            Console.ReadKey();
+            return 0;
+        }
+    }    }
