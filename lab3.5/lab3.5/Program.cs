81d1a3f513c4d5550a8d4cf2c66a9b6cbd9a8d02
revoboy <88243401+revoboy@users.noreply.github.com>
2021-08-09 11:31:14 +0300

    Add files via upload

---
+++ lab3.5/lab3.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+
+            {
+                Console.WriteLine("Корень из 0." + i +" = " + Math.Sqrt(i*0.1));
+   
+            }
+        }
+    }
+}
