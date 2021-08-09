3de3554b8af43caf7fb35aaaddbe63ac0ce870a4
revoboy <88243401+revoboy@users.noreply.github.com>
2021-08-09 11:34:08 +0300

    Add files via upload

---
+++ lab 3.4/lab 3.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтобы вычислить у = cos x + cos x^2 + cos x^3 + ... + cos x^n");            Console.WriteLine();
+            Console.WriteLine("Введите предельное значение степени от 4(четырёх). (n):");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Введите значени (x):");
+            uint x = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            double y = 0;
+            for (uint i = 1; i < n; i++)
+            {
+                y = y + Math.Cos(x ^ i);
+               
+            }
+
+            Console.WriteLine("cos x + cos x^2 + cos x^3 + ... + cos x^" + n + " = " + y);
+
+
+
+        }
+    }
+}
