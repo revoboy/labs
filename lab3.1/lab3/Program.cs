f4bf8b2d23cf5cb4d90e6b6688baf66426e07842
revoboy <88243401+revoboy@users.noreply.github.com>
2021-08-09 11:29:38 +0300

    Add files via upload

---
+++ lab3.1/lab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("enter pcs in $");
+            string exchange = Console.ReadLine();
+            exchange = exchange.Replace(".", ",");
+            decimal ex = Convert.ToDecimal(exchange);
+
+
+            for (int pcs = 1; pcs <= 20; pcs++)
+            {
+               
+                Console.WriteLine(pcs + "$" + " =\t" + pcs * ex + " UAH") ;
+            }
+            
+            
+        }
+
+    }
+}
---
+++ lab3.2/lab3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int b = 1; b <= 20; b++)
+            {
+                Decimal g = Convert.ToDecimal(b);
+                if (b == 1)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Цена за " + b + " товара(ов) -\t" + b * 20.4 + "грн");
+                
+            }
+
+
+
+
+        }
+    }
+}
